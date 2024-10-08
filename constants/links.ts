5ead30112202bdc9346e937e827977492d0246fc
Anthony Sistilli <sistillianthony@gmail.com>
2024-10-08 19:01:12 -0400

    fix yt link

--- constants/links.ts
+++ constants/links.ts
@@ -24,7 +24,7 @@ export const items: LinkItem[] = [
   {
     type: "youtube",
     title: "Latest Youtube Video",
-    youtubeId: "lO3Fvz06lzc&t=49s",
+    youtubeId: "lO3Fvz06lzc",
   },
   {
     type: "category",
